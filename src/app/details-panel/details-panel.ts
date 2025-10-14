3b4800d5bc7ff0609238d015c641cdbd0cb8fd5d
YoelRM <munozjavier541@gmail.com>
2025-10-14 13:40:50 -0500

    Creacion de Componentes para calendario

---
+++ src/app/details-panel/details-panel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-panel',
+  imports: [],
+  templateUrl: './details-panel.html',
+  styleUrl: './details-panel.css'
+})
+export class DetailsPanel {
+
+}
