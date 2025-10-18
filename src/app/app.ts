99d65fed39b24401deed838710cd598fb6d6f206
YoelRM <munozjavier541@gmail.com>
2025-10-18 14:04:43 -0500

    Integracion Exitosa del Modo Oscuro

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { YearView } from './year-view/year-view';
 import { MonthView } from './month-view/month-view';
 
-@Component({
+
+@Component ({
   selector: 'app-root',
   imports: [RouterOutlet, CommonModule, YearView, MonthView],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
    currentView: 'year' | 'month' = 'year';
   selectedYear = 2025;
   selectedMonth = 0; // Enero por defecto
+
+  isDarkMode = false;
+
+  ngOnInit(): void {
+    // Intenta cargar el tema guardado
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+      this.isDarkMode = true;
+      document.body.classList.add('dark-theme');
+    } else {
+      this.isDarkMode = false;
+      document.body.classList.remove('dark-theme');
+    }
+  }
+
+  toggleTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-theme', this.isDarkMode);
+
+    // Guarda la preferencia en localStorage
+    localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+  }
 
   showMonth(month: number) {
     this.selectedMonth = month;
